1e2f04ce8a57498d5540644a89c2d9607b729e98
Jackson Dorsett <jackson.dorsett@wsu.edu>
2021-02-02 16:24:05 -0800

    Setup Monogame and testing suite

---
+++ EverLite/EverLite/Game1.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="Game1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace EverLite
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+
+    /// <summary>
+    /// Main game class.
+    /// </summary>
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager mGraphics;
+        private SpriteBatch mSpriteBatch;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Game1"/> class.
+        /// </summary>
+        public Game1()
+        {
+            this.mGraphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Function called at instantiation. Initialized the game object.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Loads game content into this.Content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            this.mSpriteBatch = new SpriteBatch(this.GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// Updates every game loop cycle. Used for updating game logic.
+        /// </summary>
+        /// <param name="gameTime">time passed every cycle.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                this.Exit();
+            }
+
+            // TODO: Add your update logic here
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Called every gameloop cycle. Draws entities to the screen.
+        /// </summary>
+        /// <param name="gameTime">time elapsed in cycle.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ EverLite/EverLite/Program.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace EverLite
+{
+    using System;
+
+    /// <summary>
+    /// Main program running game object.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main function calling game object.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
