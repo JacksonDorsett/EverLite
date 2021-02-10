0e6e79d40cf151344dc8fb158071e1be97e1d872
Jackson Dorsett <jackson.dorsett@wsu.edu>
2021-02-09 20:31:35 -0800

    Added abstract game state class.

---
+++ EverLite/GameScore.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="GameScore.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace EverLite
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Tracks the players current score.
+    /// </summary>
+    public class GameScore
+    {
+        private static GameScore mInstance;
+        private uint mScore;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameScore"/> class.
+        /// </summary>
+        private GameScore()
+        {
+            this.mScore = 0;
+        }
+
+
+
+        /// <summary>
+        /// Gets instance of GameScore.
+        /// </summary>
+        public static GameScore Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    mInstance = new GameScore();
+                }
+                return mInstance;
+            }
+        }
+
+
+        /// <summary>
+        /// Adds points to total score.
+        /// </summary>
+        /// <param name="points">total points earned.</param>
+        public void Add(uint points)
+        {
+            this.mScore += points;
+        }
+
+        /// <summary>
+        /// Gets the total score for the game.
+        /// </summary>
+
+
+        
+    }
+}
