00f0ed6a3b7e8f8d7ab887e5c1537c23de3aec81
Jackson Dorsett <jackson.dorsett@wsu.edu>
2021-02-09 19:37:43 -0800

    added GameClock and associated test cases

---
+++ EverLite/Utilities/GameClock.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="GameClock.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace EverLite.Utilities
+{
+    using System;
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// Game Clock stores the time.
+    /// </summary>
+    public class GameClock
+    {
+        private bool isPaused;
+        private TimeSpan mTimePassed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameClock"/> class.
+        /// </summary>
+        public GameClock()
+        {
+            this.isPaused = true;
+            this.mTimePassed = new TimeSpan(0);
+        }
+
+        /// <summary>
+        /// Gets the elapsed time for GameClock.
+        /// </summary>
+        public TimeSpan ElapsedTime
+        {
+            get
+            {
+                return this.mTimePassed;
+            }
+        }
+
+        /// <summary>
+        /// Updates the GameClock called every frame.
+        /// </summary>
+        /// <param name="gameTime">game time.</param>
+        public void Update(GameTime gameTime)
+        {
+            if (!this.isPaused)
+            {
+                this.mTimePassed += gameTime.ElapsedGameTime;
+            }
+        }
+
+        /// <summary>
+        /// Starts the GameClock if it is paused.
+        /// </summary>
+        public void Start()
+        {
+            this.isPaused = false;
+        }
+
+        /// <summary>
+        /// Pauses clock if it has started.
+        /// </summary>
+        public void Pause()
+        {
+            this.isPaused = true;
+        }
+
+        /// <summary>
+        /// Resets the clock time. Note: The clock stays in its current state.
+        /// </summary>
+        public void Reset()
+        {
+            this.mTimePassed = new TimeSpan(0);
+        }
+    }
+}
---
+++ EverLiteTests/GameClockTests.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using EverLite.Utilities;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace EverLiteTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            GameClock gc = new GameClock();
+            Assert.AreEqual(new TimeSpan(0), gc.ElapsedTime);
+            //test Update
+            GameTime gt = new GameTime(new TimeSpan(100), new TimeSpan(100));
+
+            gc.Update(gt);
+            Assert.AreEqual(new TimeSpan(0), gc.ElapsedTime);
+            //test Update
+            gc.Start();
+            gc.Update(gt);
+            Assert.AreEqual(new TimeSpan(100), gc.ElapsedTime);
+
+        }
+        [Test]
+        public void TestResetWhileUnpaused()
+        {
+            GameClock gc = new GameClock();
+            gc.Start();
+            GameTime gt = new GameTime(new TimeSpan(100), new TimeSpan(100));
+            gc.Update(gt);
+            Assert.AreEqual(new TimeSpan(100), gc.ElapsedTime);
+            gc.Reset();
+            Assert.AreEqual(new TimeSpan(), gc.ElapsedTime);
+            gc.Update(gt);
+            Assert.AreEqual(new TimeSpan(100), gc.ElapsedTime);
+        }
+        [Test]
+        public void TestResetWhilePaused()
+        {
+            GameClock gc = new GameClock();
+            gc.Start();
+            GameTime gt = new GameTime(new TimeSpan(100), new TimeSpan(100));
+            gc.Update(gt);
+            Assert.AreEqual(new TimeSpan(100), gc.ElapsedTime);
+            gc.Pause();
+            gc.Reset();
+            Assert.AreEqual(new TimeSpan(), gc.ElapsedTime);
+            gc.Update(gt);
+            Assert.AreEqual(new TimeSpan(0), gc.ElapsedTime);
+        }
+    }
+}
