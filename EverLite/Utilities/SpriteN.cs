e8033f1faf3b2bdde426d1d0719605a5a3117ea0
BobNineToes <https://github.com/BobNineToes/WSU-HoloProject.git>
2021-05-04 19:57:59 -0700

    Added new Draw method to SpriteN so that the extra players are not affected by play field rotation.

--- EverLite/Utilities/SpriteN.cs
+++ EverLite/Utilities/SpriteN.cs
@@ -14,6 +14,19 @@
 
         public Texture2D Texture { get { return this.mTexture; } }
 
+        /// <summary>
+        /// Draw extra lives on side panel.
+        /// </summary>
+        /// <param name="spriteBatch">player sprite.</param>
+        /// <param name="position">sprite draw location.</param>
+        public virtual void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            Vector2 origin = new Vector2(0, 0);
+            origin.X = this.mTexture.Width / 2;
+            origin.Y = this.mTexture.Height / 2;
+            spriteBatch.Draw(this.mTexture, position, null, Color.White, 0.2f, origin, 0.3f, SpriteEffects.None, 0);
+        }
+
         public virtual void Draw(SpriteBatch spriteBatch, Vector2 position, float scale = 1, float rotation = 0)
         {
             Vector2 origin = new Vector2(0, 0);
